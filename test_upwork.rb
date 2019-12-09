f653868d6cefb784cf821631ded73ace22bbcf79
Macarena Coyle <macarena.coyle@avature.net>
2019-12-08 22:56:26 -0300

    finish exercise

--- test_upwork.rb
+++ test_upwork.rb
@@ -28,10 +28,10 @@ driver.navigate().to(url[:url_test])
   search_submit_button = driver.find_element(:xpath, '/html/body/div[1]/div[2]/div/up-header-visitor-primary-nav/nav/div/div[2]/div[1]/up-header-search/up-c-on-click-outside/form/div/div/button[1]/span[1]')
   search_input.send_keys(data[:search_text])
  #I've used print to log info in the console. I'm used to use the gem 'logger' to log info
-  print"The user have made a search in the search input"
+  print"The user have made a search in the search input.\n"
   sleep(3)
   search_submit_button.click
-  print"The user have clicked in the submit search button"
+  print"The user have clicked in the submit search button\n"
   #Frelance search list
   sleep(5)
   #Elements
@@ -39,25 +39,45 @@ driver.navigate().to(url[:url_test])
   freelancer_title = Array.new
   freelancer_title = driver.find_elements(:xpath,'//*[@id="oContractorResults"]/div/div/section/div/div/article/div[2]/div[1]/div/div/div/div/h4')
   freelancer_title_count = freelancer_title.length
-  print "total amount of frelancers are", " ", (freelancer_title_count), ". "
-
+  print " ", "The total amount of frelancers are", " ", (freelancer_title_count), ".\n"
   freelancer_title_text = freelancer_title.map { |e| e.text.downcase }
-  #print (freelancer_title_text)
-  test = Array.new
-  test = freelancer_title.map { |word| word.include? (data[:search_text]) }
-  print (test)
+
+  #I've mave a loop with while to iterate the results
+  x = 0
+  while x <  freelancer_title_text.length  do
+   if freelancer_title_text[x].split(/[^a-zA-Z]/).include?(data[:search_text])
+       print "This frelancer has keyword in their title: ", (freelancer_title_text[x]), ".\n" 
+     else 
+      print "This frelancer has not keyword in their title: ", (freelancer_title_text[x]), ".\n"
+   end
+  x +=1 
+  end
 
   sleep(1)
   card = freelancer_name.sample
   sleep(1)
-  print"it is going to click a job title card randomly"
+  print"it is going to click a job title card randomly.\n"
   card.click
-  sleep(8)
+  sleep(6)
+  print "The user enter to the freelancer person detail"
   #Freelance detail page elements
   freelance_tags = Array.new
   sleep(2)
-  freelance_tags = driver.find_elements(:xpath, '//*[@id="oProfilePage"]/div[1]/div/cfe-profile-skills-integration/div/div/div/section/div/cfe-profile-skills/div/div[1]')
+  freelance_tags = driver.find_elements(:xpath, '//*[@id="oProfilePage"]/div[1]/div/cfe-profile-skills-integration/div/div/div/section/div/cfe-profile-skills/div/div[1]/span')
   freelance_tags_text = freelance_tags.map { |e| e.text.downcase }
-  #print(freelance_tags_text)
+  sleep(5)
+
+  #I've repeated the function of the item 7 of the exercise, it is not appropriate for a OOP model/approach it should be a function
+  y = 0
+  while y <  freelance_tags_text.length  do
+   if freelance_tags_text[y].split(/[^a-zA-Z]/).include?(data[:search_text])
+       print "This tag has keyword : ", (freelance_tags_text[y]), ".\n" 
+     else
+      print "This tag has not keyword : ", (freelance_tags_text[y]), ".\n"
+   end
+  y +=1 
+  end 
+
+  sleep(2)
 driver.close
 
